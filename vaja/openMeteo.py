5677af5750ac34c2e461706bedd4c9278d994aee
Sanid99 <luka5ric11@gmail.com>
2026-09-11 09:20:54 +0000

    api 7days temp

--- vaja/openMeteo.py
+++ vaja/openMeteo.py
@@ -1,11 +1,20 @@
-#izpiši trenutno temparaturo
+#izpiši temp za 7 dni
 import requests
-def trenutna_temp(lat,lon):
+#def tedn_temp(lat,lon):
 
-    base_url=f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&current=temperature_2m&timezone=auto&forecast_days=1"
+    #base_url=f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&daily=temperature_2m_max"
 
 
-    call=requests.get(base_url).json()
-    print(call["current"]["temperature_2m"])
+    #call=requests.get(base_url).json()
+    #print(call["daily"]["temperature_2m_max"])
 
-trenutna_temp(45.12,14.5)
+#tedn_temp(46.2389,14.3556)
+
+#izpiši največjo/najmanjšo
+def max_min(la,lo):
+
+    url=f"https://api.open-meteo.com/v1/forecast?latitude={la}&longitude={lo}&daily=temperature_2m_max,temperature_2m_min"
+    call=requests.get(url).json()
+    for i in range(7):
+        print(f"{call["daily"]["temperature_2m_max"][i]}(max) - {call["daily"]["temperature_2m_min"][i]}(min) - {call["daily"]["time"][i]}(dan)")
+max_min(46.2389,14.3556)
--- vaja/uvodAPI.py
+++ vaja/uvodAPI.py
@@ -1,7 +1,6 @@
 #Uvod v API-je
 import requests #pip install requests
 base_url="https://api.chucknorris.io/jokes/random"
-
 call=requests.get(base_url)
 #print (call.text) preveri vsebino klica
 
