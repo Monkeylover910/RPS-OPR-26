f3da2420c76bf29ef2a548f8cb341af34a2b8705
Sanid99 <luka5ric11@gmail.com>
2026-09-17 06:43:57 +0000

    temp

--- vaja/openMeteo.py
+++ vaja/openMeteo.py
@@ -17,4 +17,23 @@ def max_min(la,lo):
     call=requests.get(url).json()
     for i in range(7):
         print(f"{call["daily"]["temperature_2m_max"][i]}(max) - {call["daily"]["temperature_2m_min"][i]}(min) - {call["daily"]["time"][i]}(dan)")
-max_min(46.2389,14.3556)
+#max_min(46.2389,14.3556)
+
+
+def razlika(lan,lon):
+    url=f"https://api.open-meteo.com/v1/forecast?latitude={lan}&longitude={lon}&daily=temperature_2m_max,temperature_2m_min"
+    call=requests.get(url).json()
+    najvec=0
+    trenutno=0
+    index=0
+    temp_max=call["daily"]["temperature_2m_max"]
+    temp_min=call["daily"]["temperature_2m_min"]
+    for i in range(len(temp_min)):
+        trenutno=temp_max[i]-temp_min[i]
+        if trenutno>najvec:
+            najvec=trenutno
+            index=i
+    print(call["daily"]["time"][index],najvec)
+
+
+razlika(46.2389,14.3556)
